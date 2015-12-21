8278abdb0ec176b3ebee2800db147ab86b949842
Denis Mamrak <mamrak@gmail.com>
2015-12-21 02:49:25 +0200

    Done

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,26 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		// TBD separate js and css collecting for performance improvement
-		copy: {
-			main: {
-				files: [
-					{
-						cwd: 'bower_components/',
-						expand: true,
-						flatten: true,
-						src: [
-							'jquery/dist/jquery.min.js',
-							'slick-carousel/slick/slick.min.js',
-						],
-						dest: 'js/lib/',
-					},
-					{
-						cwd: 'bower_components/',
-						expand: true,
-						flatten: true,
-						src: ['normalize-css/normalize.css'],
-						dest: 'tmp/',
-					},
-				]
-			}
+
+		// Extracting bower assets
+		bower: {
+			dev: {
+				dest: 'tmp/',
+				fonts_dest: 'fonts/',
+				images_dest: 'img/',
+				options: {
+					keepExpandedHierarchy: false,
+				},
+			},
 		},
+
+		// Processing SCSS
 		sass: {
 			global: {
 				options: {
-					sourceMap: true,
+					sourceMap: false,
 					sourceComments: false,
-					outputStyle: 'expanded'
+					outputStyle: 'expanded',
 				},
 				files: [{
 					expand: true,
@@ -38,21 +28,42 @@ module.exports = function(grunt) {
 					src: ['*.scss'],
 					dest: 'tmp/',
 					ext: '.css'
-				}]
-			}
-		},
-		concat: {
-			options: {
-				separator: '\n\n',
+				}],
 			},
-			dist: {
+		},
+
+		// Concatenating CSS
+		concat: {
+			css: {
+				options: {
+					separator: '\n\n',
+				},
 				src: [
-					'tmp/normalize.css',
+					'tmp/reset.css',
+					'tmp/*.css',
 					'tmp/main.css',
 				],
 				dest: 'css/styles.css',
 			},
+			js: {
+				options: {
+					separator: ';\n',
+				},
+				src: ['tmp/*.js'],
+				dest: 'js/scripts.js'
+			},
 		},
+
+		// Minifying scripts
+		uglify: {
+			js: {
+				files: {
+					'js/scripts.js': 'js/scripts.js',
+				},
+			}
+		},
+
+		// Autoprefixing CSS
 		autoprefixer: {
 			dist: {
 				files: [{
@@ -61,26 +72,35 @@ module.exports = function(grunt) {
 					src: ['*.css'],
 					dest: 'css',
 					ext: '.css'
-				}]
-			}
+				}],
+			},
 		},
+
+		// Cleaning temporary files
 		clean: {
-			css: ['tmp']
+			css: ['tmp'],
 		},	
+
+		// Watching changes
 		watch: {
 			styles: {
 				files: ['sass/*.scss'],
-				tasks: ['sass', 'concat', 'autoprefixer']
-			}
-		}
+				tasks: ['sass', 'concat:css', 'autoprefixer'],
+			},
+			scripts: {
+				files: ['js/*.js'],
+				tasks: ['concat:js'],
+			},
+		},
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-bower');
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['copy', 'sass', 'concat', 'autoprefixer', 'clean']);
+	grunt.registerTask('default', ['bower', 'sass', 'concat', 'autoprefixer', 'uglify', 'clean']);
 };
